dd8c4f90d9f1fa062dadaf0debbbb30ffcdf9c43
Khyber <kkysen@gmail.com>
2018-06-16 16:14:04 -0400

    Worked on Options.

--- src/ts/polybius/Options.js
+++ src/ts/polybius/Options.js
@@ -3,9 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const React = require("react");
 const ReactDOM = require("react-dom");
 const Options = ({}) => {
-    return React.createElement("div", null);
+    ["hello", "world"].map(s => s.length);
+    return React.createElement("div", null, ["hello", "world"].map(s => React.createElement("div", null, s)));
 };
 exports.reactMain = function () {
-    ReactDOM.render(React.createElement(Options, null), document.body.appendDiv());
+    ReactDOM.render(React.createElement(Options, { options: [] }), document.body.appendDiv());
 };
 //# sourceMappingURL=Options.js.map
--- src/ts/polybius/Options.tsx
+++ src/ts/polybius/Options.tsx
@@ -1,14 +1,29 @@
 import * as React from "react";
 import {SFC} from "react";
 import * as ReactDOM from "react-dom";
+import {Path} from "./DownloadRouter";
 
 
-const Options: SFC<{}> = ({}) => {
-    return <div>
+interface RouterOptions {
+
+    destination: Path;
     
+    regex: RegExp;
+    
+    filename?: string;
+    
+    extension?: string;
+
+}
+
+
+const Options: SFC<{options: RouterOptions[]}> = ({}) => {
+    ["hello", "world"].map(s => s.length);
+    return <div>
+        {["hello", "world"].map(s => <div>{s}</div>)}
     </div>;
 };
 
 export const reactMain = function(): void {
-    ReactDOM.render(<Options/>, document.body.appendDiv());
+    ReactDOM.render(<Options options={[]}/>, document.body.appendDiv());
 };

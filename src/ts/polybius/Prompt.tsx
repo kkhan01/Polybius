e4974a57b0b610feaae8de4117e8761d66d0dc15
Khinshan Khan <kkhan@stuy.edu>
2018-06-16 23:35:30 -0400

    added materialize css, edited options to include it (working on making it pretty later), created pop up alert for conflicting rules

--- src/ts/polybius/Prompt.tsx
+++ src/ts/polybius/Prompt.tsx
@@ -2,8 +2,29 @@ import {SFC} from "react";
 import * as React from "react";
 import {DownloadAction} from "./DownloadRouter";
 
+let selectedAction: DownloadAction | null = null;
 
+function actionlist(actionarray) {
+  const listItems = actionarray.map((action, i) =>
+  <p>
+	<label>
+		<input name="group1" type="radio" {i == 0 ? checked : unchecked} onClick={() => selectedAction = action} />
+		<span>{action}</span>
+  	</label>
+  </p>
+  );
+  return (
+    <form>{listItems}</form>
+  );
+};
 
-const Prompt: SFC<{actions: DownloadAction[]}> = ({actions}) => {
-    return <div></div>;
+interface PromptProps {
+	  actions: DownloadAction[];
+	  select(action: DownloadAction): void;
+}
+
+const Prompt: SFC<PromptProps> = ({actions, select}) => {
+    return <div>It seems multiple rules can be applied. Please choose a path:<br/>{actionlist(actions)}
+    <a class="waves-effect waves-light btn" onClick={() => select(selectedAction)}>Apply</a>
+    </div>;
 };
